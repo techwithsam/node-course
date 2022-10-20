8360b29230832099ab9293b3c6e181829fb47ddf
Samuel Adekunle <samuelbeebest@gmail.com>
2022-10-20 10:14:50 +0100

    Finishing the pub template

--- routes/shop.js
+++ routes/shop.js
@@ -7,10 +7,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
-
-  // console.log("shop.js", adminData.products);
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/shop" });
 });
 
 module.exports = router;
