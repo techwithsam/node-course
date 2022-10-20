99f8e3e72455ca51bfb920e1c3fbe77af1389018
Samuel Adekunle <samuelbeebest@gmail.com>
2022-10-20 13:43:37 +0100

    Working on the Layout with Partials - EJS

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get("/", (req, res, next) => {
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
-    path: "/shop",
+    path: "/",
     hasProducts: products.length > 0,
     activeShop: true,
     productCSS: true,
