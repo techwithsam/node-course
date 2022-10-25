19bc91c242bac0297eca6589a25bc1b36312fc17
Samuel Adekunle <samuelbeebest@gmail.com>
2022-10-25 23:25:20 +0100

    The Model View Controller (MVC)

--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const path = require("path");
 
-const rootDir = require("../utils/path");
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
